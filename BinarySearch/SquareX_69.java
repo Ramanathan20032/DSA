22c8fe2729c1e217c3ab66bea70595c5eb236ba1
Ramanathan20032 <Ramanathan20032@gmail.com>
2024-08-27 12:48:23 +0530

    add : commands

--- BinarySearch/SquareX_69.java
+++ BinarySearch/SquareX_69.java
@@ -16,13 +16,16 @@ public class SquareX_69 {
             if(square == x){
                 return mid;
             }
+            // mid * mid is greater than x, it means mid is too large, so we move end to mid - 1
             else if(square > x){
                 end = mid - 1;
             }
+            // mid * mid is less than x, it means mid is too small, so we move start to mid + 1
             else{
                 start = mid + 1;
             }
         }
+        // If the exact square root is not found, the loop will exit when start exceeds end, and we return end as the integer part of the square root.
         return end;
     }
 }
